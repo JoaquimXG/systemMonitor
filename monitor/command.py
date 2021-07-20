2e3885ef881e962d626546fe7c3a1719947c6577
JoaquimXG <49433786+JoaquimXG@users.noreply.github.com>
2021-07-20 21:38:57 +0100

    Initial commit, outlining package structure

---
+++ monitor/command.py
@@ -0,0 +1,4 @@
+from . import test
+
+def main():
+    print(test()) 
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="Windows System Monitor",
+    version="0.1",
+    description="Monitors and logs basic system information",
+    author="Joaquim Gomez",
+    author_email="info@joaquimgomez.com",
+    license="MIT",
+    packages=["monitor"],
+    install_requires=["markdown", "numpy"],
+    zip_safe=False,
+    entry_points={
+        "console_scripts": ["monitor=monitor.command:main"],
+    },
+)
