50d06821fcfe72fa9045c2e2454d3a2dc4ceff5d
JoaquimXG <49433786+JoaquimXG@users.noreply.github.com>
2021-07-21 23:33:41 +0100

    Preparing Menu for displaying information

--- monitor/menu.py
+++ monitor/menu.py
@@ -1,27 +1,31 @@
-def printMenu():
-    print(30 * "-" , "MENU" , 30 * "-") 
-    print("1. Menu Option 1")
-    print("2. Menu Option 2")
-    print("3. Menu Option 3")
-    print("4. Menu Option 4")
-    print("5. Exit")
-    print(67 * "-")
+import os
+
+def printMenu(title, options):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(30 * "-" , title , 30 * "-") 
+    exit = 1
+    for i, option in enumerate(options):
+        print(f"{i+1}. {option}")
+        exit += 1
+
+    print(f"{exit}. Exit")
+    print(76 * "-")
 
 def menu():
+    prompt = "Enter your choice: "
     while True:
-        printMenu()
-        choice = input("Enter your choice [1-5]: ")
+        printMenu("System Monitor", ["Uptime", "Memory Usage", "CPU Usage", "Ping Success Rate"])
+        choice = input(prompt)
          
-        if choice==1:     
-            print("Menu 1 has been selected")
-        elif choice==2:
-            print("Menu 2 has been selected")
-        elif choice==3:
-            print("Menu 3 has been selected")
-        elif choice==4:
-            print("Menu 4 has been selected")
-        elif choice==5:
-            print("Menu 5 has been selected")
-            loop=False
-        else:
-            print("Wrong option selection. Enter any key to try again..")
+        if choice == '1':     
+            break
+        elif choice == '2':
+            break
+        elif choice == '3':
+            break
+        elif choice == '4':
+            break
+        elif choice == '5':
+            menu()
+        else: 
+            prompt = "Option not available. Enter another choice: "
